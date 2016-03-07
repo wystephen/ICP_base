e32b7220e79c7b955f496fb64630c6376b9914f4
WangYan <551619855@qq.com>
2016-03-07 18:11:19 +0800

    准备实现LPHP

--- Pointfilter_test/Pointfilter_test.cpp
+++ Pointfilter_test/Pointfilter_test.cpp
@@ -5,20 +5,22 @@
 #include <pcl\registration/icp.h>
 #include <pcl/visualization/pcl_visualizer.h>
 
-#include "PointFilter.h"
 
 
 int main()
 {
 	pcl::PointCloud<pcl::PointXYZ>::Ptr pc(new pcl::PointCloud<pcl::PointXYZ>);
 
+	pcl::PointCloud<pcl::PointXYZ>::Ptr p_src_ptr(new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::PointCloud<pcl::PointXYZ>::Ptr p_target_ptr(new pcl::PointCloud<pcl::PointXYZ>);
+
+	pcl::io::loadPCDFile<pcl::PointXYZ>("write_capture1_B.pcd", *p_src_ptr);
+	pcl::io::loadPCDFile<pcl::PointXYZ>("write_capture2_B.pcd", *p_target_ptr);
+
 	pcl::io::loadPCDFile<pcl::PointXYZ>("1.pcd", *pc);
 
-	pcl::PointCloud<pcl::PointXYZ>::Ptr end(new pcl::PointCloud<pcl::PointXYZ>);
 
-	PointFilter<pcl::PointXYZ> point_filter;
-	point_filter.setInputSource(*pc);
-	point_filter.computMassCenter();
+
 
 
 	Sleep(10000);
